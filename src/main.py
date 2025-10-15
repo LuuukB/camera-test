e08ec89a78b416b74572f130fec213776d460763
LuuukB <158037306+LuuukB@users.noreply.github.com>
2025-10-15 16:14:35 +0200

    oops fergot to remove someting

--- src/main.py
+++ src/main.py
@@ -106,7 +106,7 @@ class CameraApp(App):
         #if oak0_client is None:
         logger.warning("No OAK camera config found. Falling back to laptop webcam.")
         self.use_webcam_fallback = True
-        self.webcam = cv2.VideoCapture("/mnt/c/Users/<luukb>/Videos/test_video.mp4")
+        self.webcam = cv2.VideoCapture("/mnt/c/Users/luukb/Videos/test_video.mp4")
 
         # stream camera frames
         if self.use_webcam_fallback:
